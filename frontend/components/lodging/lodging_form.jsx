504fb129a23e7934af8bcc7a4a9456864e89866c
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-09-22 19:02:58 -0700

    Make a semi functional edit create form for lodgings

--- frontend/components/lodging/lodging_form.jsx
+++ frontend/components/lodging/lodging_form.jsx
@@ -3,23 +3,65 @@ import React from 'react';
 class LodgingForm extends React.Component {
   constructor(props) {
     super(props);
-    console.log("inside lodging form", props);
+    this.state = this.props.lodging;
   }
 
-  componentWillReceiveProps(newProps) {
-    console.log("newProps", newProps);
-    if (this.props.formType !== newProps.formType) {
-      console.log('currentprops and newprops different');
-    } else {
-      console.log('same');
+  componentWillMount() {
+    if (this.props.formType === 'Edit' && this.props.lodging === undefined) {
+      this.props.fetchLodging(this.props.match.params.lodgingId)
+      .then(() => this.setState(this.props.lodging));
     }
   }
 
+  componentWillReceiveProps(newProps) {
+    if (this.props.formType === 'Edit'
+        && newProps.formType === 'Create') {
+          console.log('edit to create');
+      this.setState({
+        title: '',
+        street: '',
+        city: '',
+        country: '',
+        image_url: '',
+        rate: 0,
+        room_type: '',
+        beds: 0,
+        bedrooms: 0,
+        bathrooms: 0,
+        guests: 0,
+        check_in: '',
+        amenities: [],
+        bio: ''
+      });
+    } else if (this.props.formType === 'Create'
+               && newProps.formType === 'Edit') {
+                 console.log('create to edit');
+      this.props.fetchLodging(newProps.match.params.lodgingId);
+    } else if (this.props.formType === 'Edit'
+               && this.props.match.params.lodgingId
+               !== newProps.match.params.lodgingId) {
+                 console.log('edit but different place');
+                 console.log(newProps.match.params.lodgingId);
+                 this.props.fetchLodging(newProps.match.params.lodgingId);
+               }
+  }
+
+
   render() {
 
+    if (this.state === null ) {
+      return (
+        <div className='lodging-form'>Loading</div>
+      );
+    }
     return (
       <div className='lodging-form'>
-        Inside the lodging form!
+        {this.props.formType} a lodging!
+
+        <form>
+          <input placeholder='title' type='text' value={this.state.title}/>
+
+        </form>
       </div>
     );
   }
--- frontend/components/lodging/lodging_form_container.js
+++ frontend/components/lodging/lodging_form_container.js
@@ -5,13 +5,35 @@ import { fetchLodging, editLodging,
          makeLodging } from '../../actions/lodging_actions';
 
 const mapStateToProps = (state, ownProps) => {
-  let formType = 'edit';
+  let formType;
+  let lodging;
+  //  = state.entities.lodgings[state.ui.lodgingDisplay],
+
   if (ownProps.match.path.slice(1) === 'lodgings') {
-    formType = 'create';
+    formType = 'Create';
+    lodging = {
+      title: '',
+      street: '',
+      city: '',
+      country: '',
+      image_url: '',
+      rate: 0,
+      room_type: '',
+      beds: 0,
+      bedrooms: 0,
+      bathrooms: 0,
+      guests: 0,
+      check_in: '',
+      amenities: [],
+      bio: ''
+    };
+  } else {
+    formType = 'Edit';
+    lodging = state.entities.lodgings[ownProps.match.params.lodgingId];
   }
   return ({
-    lodging: state.entities.lodgings[state.ui.lodgingDisplay],
-    formType
+    formType,
+    lodging
   });
 };
 // action
@@ -22,7 +44,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   }
   return ({
     fetchLodging: id => dispatch(fetchLodging(id)),
-    action
+    action: lodging => dispatch(action(lodging)) 
   });
 };
 
