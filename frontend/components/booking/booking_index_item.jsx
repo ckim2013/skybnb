28f4b6c75229e26a4d9bc0759adacc230105bc91
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-09-25 22:28:52 -0700

    Fix delete buttons for reservations

--- frontend/components/booking/booking_index_item.jsx
+++ frontend/components/booking/booking_index_item.jsx
@@ -4,15 +4,18 @@ import { Image, Transformation } from 'cloudinary-react';
 
 const BookingIndexItem = ({ booking, destroyBooking }) => {
   const { duration_of_stay, start_date, end_date, lodging,
-          total_cost, id } = booking;
+          total_cost} = booking;
+
+  const booking_id = booking.id
 
   const { title, street, city, country, guests, beds,
-          image_url, rate} = lodging;
+          image_url, rate } = lodging;
 
+  const lodging_id = lodging.id
 
   return (
     <div className='booking-index-item'>
-      <Link to={`lodgings/${id}`}>
+      <Link to={`lodgings/${ lodging_id }`}>
       <Image publicId={ image_url } cloudName='skybnb'>
         <Transformation height='275' width='350' crop='scale' />
       </Image>
@@ -25,7 +28,7 @@ const BookingIndexItem = ({ booking, destroyBooking }) => {
         <li>{ duration_of_stay } days of adventure!</li>
         <li>${ rate } x { duration_of_stay } = ${ total_cost } due</li>
         <li>
-          <button onClick={() => destroyBooking(id)} className='button'>Delete</button>
+          <button onClick={ () => destroyBooking(booking_id) } className='button'>Delete</button>
         </li>
       </ul>
     </div>
