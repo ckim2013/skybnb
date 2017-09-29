279f6dac67ee42299545eb1b7b3f91b811746094
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2017-09-28 20:40:39 -0700

    Remove errors on review form whenever the user changes screens

--- frontend/reducers/review_errors_reducer.js
+++ frontend/reducers/review_errors_reducer.js
@@ -1,4 +1,5 @@
 import * as ReviewActions from '../actions/review_actions';
+import { CLEAR_ERRORS } from '../actions/error_actions';
 
 const ReviewErrorsReducer = (state = [], action) => {
   Object.freeze(state);
@@ -7,6 +8,8 @@ const ReviewErrorsReducer = (state = [], action) => {
       return [];
     case ReviewActions.RECEIVE_REVIEW_ERRORS:
       return action.errors;
+    case CLEAR_ERRORS:
+      return [];
     default:
       return state;
   }
