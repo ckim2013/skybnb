c8a69b52ff141203d23dd8892041bda3ab22b446
chrisykim2013@gmail.com <chrisykim2013@gmail.com>
2020-08-21 14:20:36 -0700

    Remove cornifu

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,19 +5,6 @@ class Splash extends React.Component {
     this.props.history.push('/explore');
   }
 
-  componentWillMount() {
-    let pressed = [];
-    let secretCode = 'chris'
-    window.addEventListener('keyup', e => {
-      pressed.push(e.key);
-      pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-      if (pressed.join('').includes(secretCode)) {
-        let func = [sharkify_add, cornify_add][Math.floor(Math.random() * 2)];
-        func();
-      }
-    });
-  }
-
   render() {
     return (
       <div className='splash-container'>
@@ -26,7 +13,6 @@ class Splash extends React.Component {
         <div className='splash-content'>
           <h1>SKYBnB</h1>
           <div>Come stay at thousands of lodgings around the world. Or host your own.</div>
-          <p>Type 'chris' at your own risk...</p>
           <button onClick={ this.redirectToExplore.bind(this) }>Explore</button>
         </div>
       </div>
